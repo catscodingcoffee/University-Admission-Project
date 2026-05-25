a7515962c4fa368ba99db10da1bbe55b020468e2
Brandon Smith <brandon_smith3@icloud.com>
2026-05-24 20:39:34 -0700

    Add university.py to calculate mean and acceptance

---
+++ Stage-1/university.py
@@ -0,0 +1,9 @@
+# write your code here
+result1 = float(input())
+result2 = float(input())
+result3 = float(input())
+
+mean = (result1+result2+result3)/3
+print(mean)
+
+print("Congratulations, you are accepted!")
